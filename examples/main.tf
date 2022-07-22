86da5dc39dd1e05136dc8f71f5f3392cdadd4f9e
Patrick Hayo <patrick.hayo@email.de>
2022-07-22 11:46:18 +0200

    updated

--- examples/main.tf
+++ examples/main.tf
@@ -2,3 +2,21 @@ resource "azurerm_resource_group" "this" {
   name     = uuid()
   location = "westeurope"
 }
+
+data "azurerm_client_config" "this" {}
+
+module "privatednszone" {
+  source               = "./module"
+  name                 = "example.mydomain.com"
+  resource_group_name  = azurerm_resource_group.this.name
+  registration_enabled = true
+  virtual_networks_to_link = {
+    ("myVnetExampleName") = {
+      subscription_id     = data.azurerm_client_config.this.subscription_id
+      resource_group_name = azurerm_resource_group.this.name
+    }
+  }
+  depends_on = [
+    module.network,
+  ]
+}
--- main.tf
+++ main.tf
@@ -15,7 +15,7 @@ locals {
   tags = merge(var.tags, local.module_tag)
 }
 
-resource "azurerm_private_dns_zone" "private_dns_zone" {
+resource "azurerm_private_dns_zone" "this" {
   name                = var.name
   resource_group_name = var.resource_group_name
   tags                = var.tags
@@ -26,12 +26,12 @@ resource "azurerm_private_dns_zone" "private_dns_zone" {
   }
 }
 
-resource "azurerm_private_dns_zone_virtual_network_link" "link" {
+resource "azurerm_private_dns_zone_virtual_network_link" "this" {
   for_each = var.virtual_networks_to_link
 
   name                  = "link_to_${lower(basename(each.key))}"
   resource_group_name   = var.resource_group_name
-  private_dns_zone_name = azurerm_private_dns_zone.private_dns_zone.name
+  private_dns_zone_name = azurerm_private_dns_zone.this.name
   virtual_network_id    = "/subscriptions/${each.value.subscription_id}/resourceGroups/${each.value.resource_group_name}/providers/Microsoft.Network/virtualNetworks/${each.key}"
   registration_enabled  = var.registration_enabled
   lifecycle {
