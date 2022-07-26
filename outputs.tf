7646c743be61e5e01da3b98831c78cc4be31642a
adminph-de <patrick.hayo@email.de>
2022-07-26 09:11:41 +0200

    fixed

--- outputs.tf
+++ outputs.tf
@@ -1,9 +1,9 @@
 output "id" {
   description = "Specifies the resource id of the private dns zone."
-  value       = azurerm_private_dns_zone.private_dns_zone.id
+  value       = azurerm_private_dns_zone.this.id
 }
 
 output "name" {
   description = "Specifies the resource id of the private dns zone."
-  value       = azurerm_private_dns_zone.private_dns_zone.name
+  value       = azurerm_private_dns_zone.this.name
 }
